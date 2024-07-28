d64c7fe2aa5d1dc5290c188c9395e3118d061aee
alstonjohnson <johnson.alston@icloud.com>
2024-07-28 15:21:27 -0500

    added api to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 
 const weatherBody = document.getElementById('weather-info');
 
+function weatherApi() {
+    const requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=32.808180&lon=-95.391150&exclude={part}&appid=43fece3568ad612b63990ffde119096a'
+        fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            const currentWeather = document.createElement('p');
+            currentWeather.textContent = 'Current: ' + data.currentConditions.conditions + ' Temp: ' + data.currentConditions.temp + 'F° Feels Like:' + data.currentConditions.feelslike + 'F°';
+            weatherBody.appendChild(currentWeather);
+
+        })
+    }
