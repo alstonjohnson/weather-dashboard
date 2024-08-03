19045e56e670c402ef6d024815db333f72263689
alstonjohnson <johnson.alston@icloud.com>
2024-08-03 16:46:11 -0500

    playing around with adding things to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,36 +10,48 @@ console.log(search);
 let units = 'imperial';
 let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=43fece3568ad612b63990ffde119096a&units=${units}`;
 fetch(requestUrl)
-        .then(function(response) {
-            return response.json();
+        .then(function(data) {
+            return data.json();
         })
         .then(function(data) {
             console.log(data);
             let row = document.querySelector('#weather-info');
+            let row2 = document.querySelector('#weather-info2');
+            let row3 = document.querySelector('#weather-info3');
+            let row4 = document.querySelector('#weather-info4');
+
             row.innerHTML = data.main.feels_like;
-                    return `<div class="col">
-            <div class="card" style="width: 30vw">
-              <h5 class="card-title p-2">Date</h5>
-              <img
-                src="http://openweathermap.org/img/wn/10d@4x.png"
-                class="card-img-top"
-                alt="Weather description"
-              />
-              <div class="card-body">
-                <h3 class="card-title">temp</h3>
-                <p class="card-text">feels like</p>
-            //     <p class="card-text">HighFeels like</p>
-            //     <p class="card-text">Pressure</p>
-            //     <p class="card-text">Humidty</p>
-            //     <p class="card-text">UV Index</p>
-            //     <p class="card-text">Precipitation</p>
-            //     <p class="card-text">Dew Point</p>
-            //     <p class="card-text">Wind speed and direction</p>
-            //     <p class="card-text">Sunrise</p>
-            //     <p class="card-text">Sunset</p>
-            //   </div>
-            </div>
-          </div>`;
+            row2.innerHTML = data.main.temp;
+            // row.innerHTML = data.main.temp_max;
+            // row.innerHTML = data.main.temp_min;
+
+            // let row2 = document.querySelector('#weather-info2');
+
+
+                    // return 
+        //             `<div class="col">
+        //     <div class="card" style="width: 30vw">
+        //       <h5 class="card-title p-2">Date</h5>
+        //       <img
+        //         src="http://openweathermap.org/img/wn/10d@4x.png"
+        //         class="card-img-top"
+        //         alt="Weather description"
+        //       />
+        //       <div class="card-body">
+        //     //     <h3 class="card-title">temp</h3>
+        //     //     <p class="card-text">feels_like</p>
+        //     //     <p class="card-text">temp_max</p>
+        //     //     <p class="card-text">temp_min</p>
+        //     //     <p class="card-text">Humidty</p>
+        //     //     <p class="card-text">UV Index</p>
+        //     //     <p class="card-text">Precipitation</p>
+        //     //     <p class="card-text">Dew Point</p>
+        //     //     <p class="card-text">Wind speed and direction</p>
+        //     //     <p class="card-text">Sunrise</p>
+        //     //     <p class="card-text">Sunset</p>
+        //     //   </div>
+        //     </div>
+        //   </div>`;
           
             }
             )
