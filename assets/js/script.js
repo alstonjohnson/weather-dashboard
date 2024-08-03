611ea12ecc7c939aa64c027a9292cefdb30cdbeb
alstonjohnson <johnson.alston@icloud.com>
2024-08-03 10:26:16 -0500

    commented out unnecessary code for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,26 +14,27 @@ fetch(requestUrl)
         })
         .then(function(data) {
             console.log(data);
-        })
-}
-
-
-
-function weatherApi() {
-    console.log(search);
-    const requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=32.808180&lon=--95.391150&appid=e43fec3568ad612b63990ffde119096a'
-        fetch(requestUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
             const currentWeather = document.createElement('table');
             currentWeather.textContent = 'Current: ' + data.currentConditions.conditions + ' Temp: ' + data.currentConditions.temp + 'F° Feels Like:' + data.currentConditions.feelslike + 'F°';
             weatherBody.appendChild(currentWeather);
 
         })
-    }
+}
+
+
+
+// function weatherApi() {
+//     console.log(search);
+//     const requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=32.808180&lon=--95.391150&appid=e43fec3568ad612b63990ffde119096a'
+//         fetch(requestUrl)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             console.log(data);
+            
+    //     })
+    // }
 
 // weatherAPI ();
 
