319fe5dad8a12b03113ff63a3db5c4db92e845ec
alstonjohnson <johnson.alston@icloud.com>
2024-08-04 13:26:20 -0500

    added humidity data to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,8 +32,8 @@ fetch(requestUrl)
                  alt="Weather description"
                />
                <div class="card-body">
-                  <h3 class="card-title">Temp ${day.main.temp}</h3>
-                  <p class="card-text">Humidty</p>
+                  <h3 class="card-title">Temp - ${day.main.temp}</h3>
+                  <p class="card-text">Humidty - ${day.main.humidity}</p>
                   <p class="card-text">Wind speed and direction</p>
                 </div>
              </div>
